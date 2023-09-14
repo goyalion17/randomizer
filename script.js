03a7e4c26f566a286c03ebf74b3802bcfdf8b992
Irina Saveleva <goyalion17@gmail.com>
2023-09-14 14:12:34 +0200

    fixed bug from 0, updated design

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const inputMax = document.querySelector(".max");
 const result = document.querySelector(".result");
 
 btn.addEventListener("click", function (min, max) {
-  min = inputMin.value;
-  max = inputMax.value;
-  let res = Math.trunc(Math.random() * (max - min) + min);
-  return (result.textContent = res);
+  min = parseInt(inputMin.value);
+  max = parseInt(inputMax.value);
+  let res = Math.trunc(Math.random() * (max - min + 1) + min);
+  if (!isNaN(min) && !isNaN(max)) {
+    result.textContent = res;
+  } else {
+    result.textContent = "";
+  }
 });
