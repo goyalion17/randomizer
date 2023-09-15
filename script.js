c338e08bb0178fb44552411405a2feeded18c8c3
Irina Saveleva <goyalion17@gmail.com>
2023-09-16 00:32:21 +0200

    refactoring, conditional operator

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const btn = document.querySelector(".button");
 const inputMin = document.querySelector(".min");
 const inputMax = document.querySelector(".max");
 const result = document.querySelector(".result");
+const inp = document.querySelectorAll(".inp");
 
 const randomBtn = function (min, max) {
   min = parseInt(inputMin.value);
   max = parseInt(inputMax.value);
-  let res = Math.trunc(Math.random() * (max - min + 1) + min);
-  if (!isNaN(min) && !isNaN(max)) {
-    result.textContent = res;
-  } else {
-    result.textContent = "";
-  }
+  const res = Math.trunc(Math.random() * (max - min + 1) + min);
+  !isNaN(min) && !isNaN(max)
+    ? (result.textContent = res)
+    : (result.textContent = "");
 };
 
 btn.addEventListener("click", randomBtn);
